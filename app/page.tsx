d5679e040c0119a25e225dbc17ac1cb40a5443f0
AswinJ1 <aswinjhome@gmail.com>
2025-10-01 23:39:25 +0530

    fix: changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,20 @@ import Navigation from "@/components/navigation"
 import HeroSection from "@/components/hero-section"
 import ServicesSection from "@/components/services-section"
 import CaseStudiesSection from "@/components/case-studies-section"
-import TestimonialsSection from "@/components/testimonials-section"
+import TestimonialsMarquee from "@/components/testimonials-section"
 import CTASection from "@/components/cta-section"
 import Footer from "@/components/footer"
 import FAQSection from "@/components/faq"
+import StatsSection from "@/components/stats"
 export default function HomePage() {
   return (
     <main className="min-h-screen">
       {/* <Navigation /> */}
       <HeroSection />
+      <StatsSection />
       <ServicesSection />
       <CaseStudiesSection />
-      <TestimonialsSection />
+      <TestimonialsMarquee />
         <FAQSection />
       <CTASection />
     
