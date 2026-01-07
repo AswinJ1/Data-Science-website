ec15b43d6c2f65ecb2cffadd2ddc46ec9b8adbdd
AswinJ1 <aswinjhome@gmail.com>
2026-01-07 14:59:55 +0530

    feat:update the website landing page

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
       <TechSolutions />
       {/* <CaseStudiesSection /> */}
       <Sponsor />
-      <TestimonialCarousel />
+      {/* <TestimonialCarousel /> */}
       {/* <TestimonialsMarquee /> */}
         <FAQSection />
       <CTASection />
